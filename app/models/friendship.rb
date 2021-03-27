051ee4c6f4b0575102a18805b69f72eee3f86d7c
Tristan Hörler <tristan.horler@gmail.com>
2021-03-27 11:35:04 +0100

    make model of Friendship

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,6 @@
 class Friendship < ApplicationRecord
-  belongs_to :user
-  belongs_to :user
+  belongs_to :user, class_name: "User"
+  belongs_to :friend, class_name: "User"
+
+  enum status: [:pending, :rejected, :accepted]
 end
