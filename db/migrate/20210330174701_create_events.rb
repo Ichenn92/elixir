a3934cdabcc4fc6ef92dccb7546cc414890faac1
Tristan Hörler <tristan.horler@gmail.com>
2021-03-30 19:49:58 +0200

    add migration for events

---
+++ db/migrate/20210330174701_create_events.rb
@@ -0,0 +1,12 @@
+class CreateEvents < ActiveRecord::Migration[6.1]
+  def change
+    create_table :events do |t|
+      t.references :activity, null: false, foreign_key: true
+      t.datetime :start_time
+      t.datetime :end_time
+      t.integer :status
+
+      t.timestamps
+    end
+  end
+end
