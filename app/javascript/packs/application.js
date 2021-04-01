b9f37d5b2f34af5cb804f92506d0540125aad7dd
Tristan Hörler <tristan.horler@gmail.com>
2021-04-01 15:35:50 +0200

    fix fa clock

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -40,7 +40,21 @@ document.addEventListener('turbolinks:load', () => {
   initNavBar();
   initSearchNavbarToggle();
   initChatgroupCable();
+  
+  $.fn.datetimepicker.Constructor.Default = $.extend({},
+    $.fn.datetimepicker.Constructor.Default,
+    { icons:
+            { time: 'fas fa-clock',
+                date: 'fas fa-calendar',
+                up: 'fas fa-arrow-up',
+                down: 'fas fa-arrow-down',
+                previous: 'fas fa-arrow-circle-left',
+                next: 'fas fa-arrow-circle-right',
+                today: 'far fa-calendar-check-o',
+                clear: 'fas fa-trash',
+                close: 'far fa-times' } });
   $('#datetimepicker1').datetimepicker();
+
 });
 
 
