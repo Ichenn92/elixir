305837b5870d36d12fa6a615c547414c3cc73496
Tristan Hörler <tristan.horler@gmail.com>
2021-04-02 00:07:34 +0200

    create migration for booking

---
+++ db/migrate/20210401220648_create_bookings.rb
@@ -0,0 +1,11 @@
+class CreateBookings < ActiveRecord::Migration[6.1]
+  def change
+    create_table :bookings do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :event, null: false, foreign_key: true
+      t.integer :status, default: 0
+
+      t.timestamps
+    end
+  end
+end
