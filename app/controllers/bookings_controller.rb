a074082fb35f6b22021c715a0050978c65aedc10
Tristan Hörler <tristan.horler@gmail.com>
2021-04-02 01:27:57 +0200

    add update booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,22 @@
 class BookingsController < ApplicationController
   def create
     @booking = Booking.new()
-    @event = Event.find(params[:event_id])
+    @event = Event.find(params[:booking][:event])
     @booking.event = @event
     @booking.user = current_user
 
     flash.alert = "Un problème est survenu" unless @booking.save
-    redirect_back fallback_location: activities_path()
+    redirect_to activity_path(@event.activity), anchor: "event-#{@event.id}"
+  end
+
+  def update
+    puts "i am in update"
+    @booking = Booking.find(params[:id])
+    if @booking.confirmed?
+      @booking.canceled!
+    else
+      @booking.confirmed!
+    end
+    redirect_to activity_path(@booking.event.activity, anchor: "event-#{@booking.event.id}")
   end
 end
