de100edbedf793d9cab99ecf95357912dc090f99
Tristan Hörler <tristan.horler@gmail.com>
2021-04-01 18:38:39 +0200

    clean test event controller

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,8 +1,4 @@
 require "test_helper"
 
 class EventsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get events_create_url
-    assert_response :success
-  end
 end
