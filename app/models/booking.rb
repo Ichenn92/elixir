01e047da48354f35ac269745f679b6646a77bce0
Tristan Hörler <tristan.horler@gmail.com>
2021-04-02 03:16:31 +0200

    fix logic and refactor

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,7 @@ class Booking < ApplicationRecord
 
   # if create the logic of pending, pending must be at index 0 (default value)
   enum status: [:confirmed, :canceled]
+
+  scope :all_confirmed, -> { where("status = ?", 0) }
+  scope :all_canceled, -> { where("status = ?", 1) }
 end
