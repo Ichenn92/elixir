edfec0b53263153185e4a24fc0836885f0189f8f
Michael Laurac <michaellaurac@gmail.com>
2021-04-03 16:33:54 +0200

    Categorizations and labelings created and seeded in the database

---
+++ db/migrate/20210403122803_create_labelings.rb
@@ -0,0 +1,9 @@
+class CreateLabelings < ActiveRecord::Migration[6.1]
+  def change
+    create_table :labelings do |t|
+      t.references :label, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
