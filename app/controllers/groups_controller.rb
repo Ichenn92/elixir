72897e10d224d4ddf4b5bff6708e10a9d4564a87
Tristan Hörler <tristan.horler@gmail.com>
2021-04-04 09:29:49 +0200

    add logic for separating the groups friends and groups activity in view

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,16 +1,15 @@
 class GroupsController < ApplicationController
-
   def show
     @group = Group.find(params[:id])
     @messages = @group.messages
     @message = Message.new
     @groups = current_user.groups
     users = @group.users
-    @friend = users.where.not( email: current_user.email).first
+    @friend = users.where.not(email: current_user.email).first
   end
 
   def index
-    @groups = current_user.groups
+    @activity_groups = current_user.groups.where(group?: true)
+    @friendship_groups = current_user.groups.where(group?: false)
   end
-
 end
