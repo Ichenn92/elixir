851c7cc002612756d50517282abcedcc8063954f
Tristan Hörler <tristan.horler@gmail.com>
2021-04-04 14:43:14 +0200

    fix message when send content blank

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,10 @@
 class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
+
+  validates :content, {
+    :presence => true,
+    :allow_blank => false,
+    :allow_nil => false,
+  }
 end
