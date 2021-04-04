afecefa5225b7a530c0e9351dd11f5fe622ba991
Tristan Hörler <tristan.horler@gmail.com>
2021-04-04 15:13:08 +0200

    notifications on group

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,16 @@ class Group < ApplicationRecord
   has_many :memberships, dependent: :delete_all
   has_many :users, through: :memberships
   belongs_to :activity, optional: true
+
+  def unread?(user)
+    membership = self.memberships.find_by(user: user)
+    last_visit = membership.last_visit
+
+    return false if self.messages.empty?
+    last_message = self.messages.last.created_at
+    p "yooooo"
+    p last_visit
+    p last_message
+    return last_visit < last_message
+  end
 end
