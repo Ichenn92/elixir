2b6f4a50d252aa5c26a1b72256648b3b1d3071fd
Tristan Hörler <tristan.horler@gmail.com>
2021-04-04 17:13:16 +0200

    trigger remove notification on loading page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :notification_invitations
   before_action :save_last_visit_on_group_chat
+  before_action :save_current_visit_on_group_chat
   before_action :notification_messages_all_groups
 
   def configure_permitted_parameters
@@ -20,6 +21,20 @@ class ApplicationController < ActionController::Base
     group_id = previous_path[1].to_i
     previous_page_is_group_chat = previous_path.first == "groups" && group_id != 0
     if current_user && previous_page_is_group_chat
+      @notifications_messages_all_groups = 0
+      @group = Group.find(group_id)
+      save_last_visit_on_group_chat_helper(current_user, @group)
+    end
+  end
+
+  def save_current_visit_on_group_chat
+    return if request.env["PATH_INFO"].nil?
+    p "lasttt"
+    p request.env["PATH_INFO"]
+    current_path = URI(request.env["PATH_INFO"]).path.split("/").reject { |c| c.empty? }
+    group_id = current_path[1].to_i
+    current_page_is_group_chat = current_path.first == "groups" && group_id != 0
+    if current_user && current_page_is_group_chat
       @notifications_messages_all_groups = 0
       @group = Group.find(group_id)
       save_last_visit_on_group_chat_helper(current_user, @group)
