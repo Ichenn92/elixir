0f8011ec1b47e7dffbe169538b5216be02d68e67
Tristan Hörler <tristan.horler@gmail.com>
2021-04-04 14:04:00 +0200

    add update last visit on memberships

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :save_last_visit_on_group_chat
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
@@ -13,16 +14,28 @@ class ApplicationController < ActionController::Base
   before_action :notification_messages_all_groups
 
   def notification_messages_all_groups
-    if current_user
-      @notifications_messages_all_groups = 0
+  end
 
-      current_user.groups.each do |group|
-      end
-      @notification_invitations = current_user.friendships.pending_invitations.select do |friendship|
-        friendship.user if friendship.friend == current_user
-      end.map { |friendship| friendship.user }
-    else
-      @notification_invitations = []
+  #---------------------------------------------------------------
+
+  def save_last_visit_on_group_chat
+    previous_path = URI(request.referrer).path.split("/").reject { |c| c.empty? }
+    group_id = previous_path[1].to_i
+    previous_page_is_group_chat = previous_path.first == "groups" && @group_id != 0
+
+    if current_user && previous_page_is_group_chat
+      @notifications_messages_all_groups = 0
+      @group = Group.find(group_id)
+      save_last_visit_on_group_chat_helper(current_user, @group)
     end
+  end
+
+  private
+
+  def save_last_visit_on_group_chat_helper(user, group_id)
+    @membership = Membership.find_by(user: user, group: @group)
+    now = DateTime.now
+    @membership.last_visit = now
+    @membership.save
   end
 end
