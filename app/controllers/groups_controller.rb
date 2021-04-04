e5cebce20b2ca441d9962f14137d74007e2f200f
Tristan Hörler <tristan.horler@gmail.com>
2021-04-04 14:38:18 +0200

    fix groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,9 @@
 class GroupsController < ApplicationController
+  before_action :set_groups, only: [:index, :show]
+
+  def index
+  end
+
   def show
     @group = Group.find(params[:id])
     @messages = @group.messages
@@ -8,8 +13,10 @@ class GroupsController < ApplicationController
     @friend = users.where.not(email: current_user.email).first
   end
 
-  def index
-    @activity_groups = current_user.groups.where(group?: true)
+  private
+
+  def set_groups
     @friendship_groups = current_user.groups.where(group?: false)
+    @activity_groups = current_user.groups.where(group?: true)
   end
 end
