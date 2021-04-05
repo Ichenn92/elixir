d71d954187ab3cf3ed622fe941be9613968ef447
Tristan Hörler <tristan.horler@gmail.com>
2021-04-05 02:59:58 +0200

    add button cancel request friendship

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -13,12 +13,14 @@ class FriendshipsController < ApplicationController
     @friendship.user = current_user
     @friendship.friend = @friend
     @friendship.save
+    flash.notice = "Votre demande d'amitié à été envoyé à @#{@friend.nickname}"
     redirect_back fallback_location: root_path
   end
 
   def destroy
-    puts params
     @friendship = Friendship.find(params[:id])
+    user = @friendship.user == current_user ? @friendship.friend : @friendship.user
+    flash.notice = "Vous avez retiré @#{user.nickname} de votre liste d'amis"
     @friendship.destroy
     redirect_back fallback_location: root_path
   end
@@ -26,12 +28,15 @@ class FriendshipsController < ApplicationController
   def accept
     @friendship = Friendship.find(params[:friendship_id])
     @friendship.accepted!
+    flash.notice = "Vous êtes maintenant ami avec @#{@friendship.user.nickname}"
     redirect_back fallback_location: root_path
   end
 
   def reject
     @friendship = Friendship.find(params[:friendship_id])
-    @friendship.rejected!
+    user = @friendship.user == current_user ? @friendship.friend : @friendship.user
+    flash.notice = "Vous avez refusé @#{user.nickname}"
+    @friendship.destroy
     redirect_back fallback_location: root_path
   end
 end
