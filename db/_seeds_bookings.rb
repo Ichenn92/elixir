0a3972ec3e09e007de1d2048f3f12057f3ca64e8
Clementine Dubuis <clementine.dubuis.mail@gmail.com>
2021-04-08 11:37:23 +0200

    seeds events and bookings échec

--- db/_seeds_bookings.rb
+++ db/_seeds_bookings.rb
@@ -4,7 +4,7 @@ module SEED_BOOKINGS
   BOOKINGS = [
     {
       event_id: 1,
-      user_id: 12,
+      user_id: 33,
     },
     {
       event_id: 1,
@@ -49,6 +49,62 @@ module SEED_BOOKINGS
     {
       event_id: 3,
       user_id: 6,
+    },
+    {
+      event_id: 4,
+      user_id: 33,
+    },
+    {
+      event_id: 4,
+      user_id: 3,
+    },
+    {
+      event_id: 4,
+      user_id: 15,
+    },
+    {
+      event_id: 4,
+      user_id: 20,
+    },
+    {
+      event_id: 5,
+      user_id: 33,
+    },
+    {
+      event_id: 5,
+      user_id: 9,
+    },
+    {
+      event_id: 6,
+      user_id: 28,
+    },
+    {
+      event_id: 6,
+      user_id: 33,
+    },
+    {
+      event_id: 6,
+      user_id: 20,
+    },
+    {
+      event_id: 6,
+      user_id: 21,
+    },
+    {
+      event_id: 7,
+      user_id: 12,
+    },
+    {
+      event_id: 7,
+      user_id: 32,
+    },
+    {
+      event_id: 7,
+      user_id: 3,
+    },
+    {
+      event_id: 7,
+      user_id: 29,
     },
   ]
 
